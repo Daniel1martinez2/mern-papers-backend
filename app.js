d6ed62f084f5547da3050315eaae2c8a5a3a8e2d
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-03-26 23:37:04 -0500

    efe

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const mongoConnect = require('./util/database').mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
 
 const express = require('express');
 const app = express();
@@ -20,7 +20,7 @@ app.use('/', (req, res, next) => {
   })
 });
 
-mongoConnect()
-  .then(db => {
-    app.listen(port);
-  })
+app.listen(port);
+// mongoConnect()
+//   .then(db => {
+//   })
